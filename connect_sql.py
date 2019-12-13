261b9ce3cb2c20606ace50c4fcd04b72506d6c16
kajal-patel97 <57909923+kajal-patel97@users.noreply.github.com>
2019-12-13 09:13:00 +0000

    connection made into a class

--- connect_sql.py
+++ connect_sql.py
@@ -1,19 +1,24 @@
 import pyodbc
 
-#These are our variables we have to connect
-server = 'localhost, 1433'
-database = 'airport_python'
-user_name = 'SA'
-password = 'Passw0rd2018'
+class MSDBConnection():
 
-#Making the connection
+    def __init__(self):
+        self.server = 'localhost, 1433'
+        self.database = 'airport_python'
+        self.user_name = 'SA'
+        self.password = 'Passw0rd2018'
 
-docker_airport_python = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+user_name+';PWD='+ password)
 
-#Making a cursor
-cursor = docker_airport_python.cursor() # creates an object which is a cursor
+        #Making the connection
 
-cursor.execute("SELECT * FROM passengers")
-row = cursor.fetchone()
-print(row)
+        self.docker_airport_python = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+user_name+';PWD='+ password)
+
+        #Making a cursor
+        self.cursor = self.docker_airport_python.cursor() # creates an object which is a cursor
+
+        #method for sql query
+    def __sql_query(self, sql_query):
+        return self.cursor.execute(sql_query)
+
+
 
